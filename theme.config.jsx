7465d3de12ad5aeaf1e54114f796d00c47438d86
rickkdev <l.riccardo@web.de>
2024-03-18 11:18:40 +0100

    adjusted seo for help page

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,18 +1,10 @@
-// Assuming this file is a .js or .jsx file that supports JSX syntax
 import Image from "next/image";
 import Footer from "@/components/sales/footer";
 import CustomSidebarTitle from "@/components/sales/custom-sidebar-title";
 import { NextSeo } from "next-seo";
 
 const themeConfig = {
-  logo: (
-    <Image
-      src="/logo.png"
-      alt="OpenQ"
-      width={125} // Original width of the image
-      height={75} // Original height of the image
-    />
-  ),
+  logo: <Image src="/logo.png" alt="OpenQ" width={125} height={75} />,
   footer: {
     component: <Footer />,
   },
@@ -26,8 +18,31 @@ const themeConfig = {
   docsRepositoryBase: "https://github.com/OpenQDev/openq-feedback",
   useNextSeoProps() {
     return {
-      titleTemplate: "OpenQ | %s",
-      description: "OpenQ help center portal",
+      titleTemplate: "%s | OpenQ Help Center",
+      description:
+        "Discover comprehensive support and in-depth guides on OpenQ - your CRM for developer relations. Whether you're looking for feature references, detailed tutorials, or answers to FAQs, our Help Page offers everything you need to maximize your OpenQ experience.",
+      canonical: "https://www.openq.dev/help",
+      openGraph: {
+        url: "https://www.openq.dev/help",
+        title: "%s | OpenQ Help Center",
+        description:
+          "Discover comprehensive support and in-depth guides on OpenQ - your CRM for developer relations. Whether you're looking for feature references, detailed tutorials, or answers to FAQs, our Help Page offers everything you need to maximize your OpenQ experience.",
+        images: [
+          {
+            url: "https://www.openq.dev/openq-website-thumbnail.png",
+            width: 1200,
+            height: 630,
+            alt: "OpenQ Help Center",
+            type: "image/png",
+          },
+        ],
+        siteName: "OpenQ",
+      },
+      twitter: {
+        handle: "@openqlabs",
+        site: "@openqlabs",
+        cardType: "summary_large_image",
+      },
     };
   },
   darkMode: false,
