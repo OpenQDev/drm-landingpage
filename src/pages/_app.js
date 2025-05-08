6921a678f8263fe5eee8ee516c3b92ac1157866b
Riccardo Lamanna <l.riccardo@web.de>
2025-05-08 15:56:11 +0200

    adjut ganalytics to use next third parties package

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,24 +1,13 @@
 import "@/styles/globals.css";
-import Script from "next/script";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import CookieBanner from "@/components/CookieBanner";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Script
-        strategy="lazyOnload"
-        src="https://www.googletagmanager.com/gtag/js?id=G-HWEBVEMW1L"
-      />
-      <Script strategy="lazyOnload" id="google-analytics-config">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-HWEBVEMW1L');
-        `}
-      </Script>
       <Component {...pageProps} />
       <CookieBanner />
+      <GoogleAnalytics gaId="G-HWEBVEMW1L" />
     </>
   );
 }
