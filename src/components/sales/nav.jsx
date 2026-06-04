0e9ac000289c802b2f2e946514d72a3a8d509308
Riccardo Lamanna <l.riccardo@web.de>
2026-06-04 13:55:47 +0200

    Fine tune blog nav padding

--- src/components/sales/nav.jsx
+++ src/components/sales/nav.jsx
@@ -23,7 +23,7 @@ const Subnav = () => {
             </Link>
             <div className="hidden h-full md:flex items-center">
               <Link href="/blog" className="flex h-full items-center">
-                <span className="pb-2 font-semibold leading-none text-gray-700">
+                <span className="pb-1.5 font-semibold leading-none text-gray-700">
                   Blog
                 </span>
               </Link>
